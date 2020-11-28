bcddbb6e7ac6c1e699c840cacb95ddc7bbb79221
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-11-28 23:13:53 +0700

    add route saved-recipes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Recipes from './page/Recipes'
 import DetailRecipes from './page/DetailRecipes'
 import Home from './page/Home'
 import About from './page/About'
+import SaveRecipes from './page/SaveRecipes'
 
 function App() {
   return (
       <Router>
         <Switch>
           <Route path="/detail-recipes/:key" component={DetailRecipes}/>
+          <Route path="/saved-recipes" component={SaveRecipes}/>
           <Route path="/recipes" component={Recipes}/>
           <Route path="/about" component={About}/>
           <Route path="/" component={Home}/>
