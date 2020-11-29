ed90387877e836259677959473f3019e433713cc
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-11-29 23:15:22 +0700

    add ref and recipes list

--- src/page/home.js
+++ src/page/home.js
@@ -1,28 +1,50 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import {useRef} from 'react'
+
 import Navbar from '../components/navbar'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 
-import FoodImg from '../assets/images/food-ex.jpg'
+import {ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+
+import {BeatLoader} from 'react-spinners'
+import {useRecipes} from '../context/recipes-context'
+
 
 export default function Home() {
+    const {recipes,status,handleSave,notify}=useRecipes()
+    const refRecipes=useRef()
     return (
         <>
             <Navbar/>
-            <Hero/>
+            <Hero refRecipes={refRecipes}/>
             <div className="container mx-auto px-5 mt-12 ">
-                <div className="title-section ">
+                <div className="title-section " ref={refRecipes}>
                     # Resep Hari Ini
                 </div>
                 <div className="flex flex-col mb-5">
+                    {status==="success" ?
                     <div className="w-full grid lg:grid-cols-3 gap-4">
-                    <div className="h-auto card border-2 border-yellow-500">
+                        {recipes.slice(0,3).map((recipe,index)=>{
+                            return(
+                                <div key={index}  className="h-auto card border-2 border-yellow-500">
                                 <div className="flex justify-end">
-                                    <button className="px-3 py-1 absolute -mt-6 bg-red-500 text-white font-bold">+</button>
+                                    <button 
+                                    className={`px-3 py-1 absolute -mt-6 
+                                    bg-red-500 text-white font-bold
+                                    disabled:opacity-50
+                                    `}
+                                    onClick={()=>{
+                                        handleSave(recipe)
+                                        notify()
+                                    }}
+                                    >+</button>
                                 </div>
-                                <img className="h-40 w-full object-cover rounded-md" src={FoodImg} alt="food-one"/>
+                                <Link to={`detail-recipes/${recipe.key}`} thumb={recipe.thumb}>
+                                <img className="h-40 w-full object-cover rounded-md" src={recipe.thumb} alt={recipe.key}/>
                                 <div className="text-center w-full pt-4 font-semibold">
-                                    Resep Udang Galah Santan Kecap, Siapkan untuk Momen Spesialmu
+                                    {recipe.title}
                                 </div>
                                 <div className="divide-y divide-gray-400">
                                     <div className="text-center py-2"></div>
@@ -31,76 +53,38 @@ export default function Home() {
                                 <div className="flex flex-row justify-between items-center text-center px-5 mb-4  font-semibold">
                                     <div className="flex flex-col">
                                         <div className="text-sm text-gray-500">Waktu Masak</div>
-                                        <div>1jam 45mnt</div>
+                                        <div>{recipe.times}</div>
                                     </div>
                                     <div className="flex flex-col mx-3">
                                         <div className="text-sm text-gray-500">Porsi</div>
-                                        <div>8 Porsi</div>
+                                        <div>{recipe.portion}</div>
                                     </div>
                                     <div className="flex flex-col">
                                         <div className="text-sm text-gray-500">Level</div>
-                                        <div>Cukup Rumit</div>
+                                        <div>{recipe.dificulty}</div>
                                     </div>
                                 </div>
+                            </Link>
                             </div>
-                    <div className="h-auto card border-2 border-yellow-500">
-                                <div className="flex justify-end">
-                                    <button className="px-3 py-1 absolute -mt-6 bg-red-500 text-white font-bold">+</button>
-                                </div>
-                                <img className="h-40 w-full object-cover rounded-md" src={FoodImg} alt="food-one"/>
-                                <div className="text-center w-full pt-4 font-semibold">
-                                    Resep Udang Galah Santan Kecap, Siapkan untuk Momen Spesialmu
-                                </div>
-                                <div className="divide-y divide-gray-400">
-                                    <div className="text-center py-2"></div>
-                                    <div className="text-center py-2"></div>
-                                </div>
-                                <div className="flex flex-row justify-between items-center text-center px-5 mb-4  font-semibold">
-                                    <div className="flex flex-col">
-                                        <div className="text-sm text-gray-500">Waktu Masak</div>
-                                        <div>1jam 45mnt</div>
-                                    </div>
-                                    <div className="flex flex-col mx-3">
-                                        <div className="text-sm text-gray-500">Porsi</div>
-                                        <div>8 Porsi</div>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        <div className="text-sm text-gray-500">Level</div>
-                                        <div>Cukup Rumit</div>
-                                    </div>
-                                </div>
-                            </div>
-                    <div className="h-auto card border-2 border-yellow-500">
-                                <div className="flex justify-end">
-                                    <button className="px-3 py-1 absolute -mt-6 bg-red-500 text-white font-bold">+</button>
-                                </div>
-                                <img className="h-40 w-full object-cover rounded-md" src={FoodImg} alt="food-one"/>
-                                <div className="text-center w-full pt-4 font-semibold">
-                                    Resep Udang Galah Santan Kecap, Siapkan untuk Momen Spesialmu
-                                </div>
-                                <div className="divide-y divide-gray-400">
-                                    <div className="text-center py-2"></div>
-                                    <div className="text-center py-2"></div>
-                                </div>
-                                <div className="flex flex-row justify-between items-center text-center px-5 mb-4  font-semibold">
-                                    <div className="flex flex-col">
-                                        <div className="text-sm text-gray-500">Waktu Masak</div>
-                                        <div>1jam 45mnt</div>
-                                    </div>
-                                    <div className="flex flex-col mx-3">
-                                        <div className="text-sm text-gray-500">Porsi</div>
-                                        <div>8 Porsi</div>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        <div className="text-sm text-gray-500">Level</div>
-                                        <div>Cukup Rumit</div>
-                                    </div>
-                                </div>
-                            </div>
+                            )
+                        })}
+                        
                     </div>
+                    :<div className="text-4xl text-center mt-12 mb-12">
+                        <BeatLoader
+                        color="black"
+                        margin={5}
+                        size={25}
+                        />
+                    </div>
+                }
                     <div className="w-full mx-3">
                         <div className="h-40 text-center flex justify-center">
-                            <button className="px-3 py-2 text-2xl text-white font-bold bg-yellow-500 self-center shadow-2xl">Lebih Banyak Resep {`>`} </button>
+                                <button className="px-3 py-2 text-2xl text-white font-bold bg-yellow-500 self-center shadow-2xl">
+                                    <Link to="/recipes">
+                                        Lebih Banyak Resep {`>`} 
+                                    </Link>
+                                </button>
                         </div>
                     </div>
                 </div>
@@ -122,6 +106,7 @@ export default function Home() {
                         <p className="capitalize">Simpan nanti adalah fitur untuk menandai resep yang akan user coba dikemudian hari, dengan menekan icon plus pada tiap card resep yang di tampilkan </p>
                     </div>
                 </div>
+            <ToastContainer/>
             </div>
             <Footer/>
         </>
